f16c8fb35878f272ee91310e4c3d6c60f1c6e037
Samuel <samuel1skultety@gmail.com>
2023-01-07 14:55:53 +0100

    feat: basic calculator functionality implemented

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let operation = "";
+const operations = ["+", "-", "*", "/", "=", "Clear", "Del", "."];
+let num1 = 0;
+let num2 = 0;
+
 const add = (a, b) => a + b;
 
 const subtract = (a, b) => a - b;
@@ -21,3 +26,57 @@ const operate = (operator, a, b) => {
         return divide(a, b);
     }
 }
+
+// Path: script.js
+const currDisplay = document.querySelector(".current-operand");
+const prevDisplay = document.querySelector(".previous-operand");
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        calculator(button);
+    });
+});
+
+const calculator = (e) => {
+
+    let operand = e.textContent;
+
+    if (operand === "=") {
+        num2 = parseFloat(currDisplay.textContent);
+        console.log(num1 + " num1");
+        console.log(num2 + " num2");
+        console.log(operation + " operation");
+        currDisplay.textContent = operate(operation, num1, num2);
+    }
+
+
+    if (operand === "Clear") {
+        currDisplay.textContent = "";
+        prevDisplay.textContent = "";
+        operation = "";
+    }
+
+    if (operand === "Del") {
+        currDisplay.textContent = currDisplay.textContent.slice(0, -1);
+    }
+
+    if (operand === ".") {
+        if (!currDisplay.textContent.includes(".")) {
+            currDisplay.textContent += e.textContent;
+        }
+    }
+
+    if (!operations.includes(operand)) {
+        currDisplay.textContent += e.textContent;
+    } else if (operand != "=" && operand != "Clear" && operand != "Del" && operand != ".") {
+        num1 = parseFloat(currDisplay.textContent);
+        prevDisplay.textContent = currDisplay.textContent;
+        currDisplay.textContent = "";
+        operation = operand;
+        console.log(num1);
+    }
+        
+}
+
